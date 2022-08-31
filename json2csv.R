dad0c3465bbf8bbeb1bf8eb3cb87f19bc935b971
George Moroz <agricolamz@gmail.com>
2022-08-31 22:37:20 +0300

    update

--- json2csv.R
+++ json2csv.R
@@ -47,7 +47,7 @@ map_dfr(files, function(json){
       map_chr("off_end_src") ->
       time_end
     
-    tibble(filename = json,
+    tibble(filename = source_file,
            time_start = time_start,
            time_end = time_end,
            speaker = js$sentences[[i]]$meta$speaker,
@@ -80,3 +80,5 @@ result %>%
   left_join(translation_pairs) %>%
   select(filename, time_start, time_end, speaker, recorded, sentence_id, text, translation, word_forms, morphonology, gloss, language, dataset_creator, dataset_provider, sentence_id, translation)  %>% 
   write_csv("data_oral_besleney_corpus.csv")
+
+## RUN encrypt.R
